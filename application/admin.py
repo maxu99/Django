3fd22515a6be8fb664ce9fa2376bb7180a4f4023
Cristian221995 <crisfonteina@gmail.com>
2019-11-02 14:49:14 -0300

    Modelos y migrate

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from application.models import City, Owner_Ship, Reservation, Date_Rent
+
+admin.site.register(City)
+admin.site.register(Owner_Ship)
+admin.site.register(Reservation)
+admin.site.register(Date_Rent)
+
--- application/models.py
+++ application/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Ciudades'
+
+    def __str__(self):
+        return self.name
+
+
+class Owner_Ship(models.Model):
+    description = models.CharField(max_length=200)
+    capacity = models.IntegerField()
+    city = models.ForeignKey(City, null=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(User, null=False, on_delete=models.SET('null'))
+    image = models.CharField(null=True, blank=True, max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Propiedades'
+
+    def __str__(self):
+        return self.description
+
+
+class Reservation(models.Model):
+    date = models.DateTimeField()
+    code = models.IntegerField()
+    total = models.IntegerField()
+    owner_ship = models.ForeignKey(Owner_Ship, null=False, on_delete=models.SET('null'))
+
+    class Meta:
+        verbose_name_plural = 'Reservas'
+
+    def __str__(self):
+        return self.date, self.total
+
+
+class Date_Rent(models.Model):
+    date = models.DateTimeField()
+    owner_ship = models.ForeignKey(Owner_Ship, null=False, on_delete=models.SET('null'))
+    reservation = models.ForeignKey(Reservation, null=False, on_delete=models.SET('null'))
+
+    class Meta:
+        verbose_name_plural = 'Fechas de Alquileres'
+
+    def __str__(self):
+        return self.date
