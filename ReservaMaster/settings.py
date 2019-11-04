30d389e1629f462d792350ba418c2bf3f20b1324
Cristian221995 <crisfonteina@gmail.com>
2019-11-03 22:44:50 -0300

    Formulario de django para ciudad y imageField para imagen de propiedad

--- ReservaMaster/settings.py
+++ ReservaMaster/settings.py
@@ -12,6 +12,20 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'media')
+
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'static-only')
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'static'),
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- application/views.py
+++ application/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout as do_logout, authenticate
 from django.contrib.auth import login as do_login
 
+from application.forms import CityForm
 from application.models import Owner_Ship, City
+
+from ReservaMaster.settings import ROOT_DIR
 
 
 def welcome(request):
@@ -12,7 +15,7 @@ def welcome(request):
     # Si estamos identificados devolvemos la portada
     if request.user.is_authenticated:
         return render(request, "application/welcome.html", {'propiedades_list': propiedades_list,
-                                                            'ciudades_list': ciudades_list})
+                                                            'ciudades_list': ciudades_list, 'root': ROOT_DIR})
     # En otro caso redireccionamos al login
     return redirect('/login')
 
@@ -62,8 +65,10 @@ def ownershipform(request):
         price = request.POST['price']
         city = request.POST['city']
         my_city = ciudades_list.filter(name=city).first()
+        image = request.POST.get('image', False)
         if name is not None:
-            p = Owner_Ship(name=name, description=description, price=price, capacity=capacity, city=my_city, owner=request.user)
+            p = Owner_Ship(name=name, description=description, price=price, capacity=capacity, city=my_city,
+                           owner=request.user, image=image)
             p.save()
             msg = 'Cargado Correctamente'
         else:
@@ -78,17 +83,17 @@ def cityform(request):
     # Si estamos identificados devolvemos la portada
     error = ''
     msg = ''
+    form = CityForm()
     if request.method == 'POST':
-        name = request.POST['cityname']
-        province = request.POST['province']
-        if name is not None:
-            c = City(name=name, province=province)
-            c.save()
+        form = CityForm(request.POST)
+        if form.is_valid():
+            city = form.save(commit=False)
+            city.save()
             msg = 'Cargado Correctamente'
         else:
             error = 'La ciudad debe tener nombre'
     if request.user.is_authenticated:
-        return render(request, "application/cityform.html", {'error': error, 'msg': msg})
+        return render(request, "application/cityform.html", {'error': error, 'msg': msg, 'form': form})
         msg = ''
     # En otro caso redireccionamos al login
     return redirect('/login')
