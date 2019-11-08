1a57d1e1eba6f57a3b935746c08038e3a5c50d2d
cristian221995 <crisfonteina@gmail.com>
2019-11-08 19:25:01 -0300

    Se muestran las imagenes

--- ReservaMaster/urls.py
+++ ReservaMaster/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from application import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.welcome),
@@ -28,3 +30,7 @@ urlpatterns = [
     path('detail/<int:owner_ship_id>', views.detail),
     path('city/<int:city_id>', views.welcome),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
